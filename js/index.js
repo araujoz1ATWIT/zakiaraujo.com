f4b88b7139bb290b5db5494548340fe6136042d1
Zaki Araujo <araujoz1@wit.edu>
2022-10-17 21:51:30 -0400

    Gallery

--- js/index.js
+++ js/index.js
@@ -22,3 +22,4 @@ AOS.init({
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
 });
+
